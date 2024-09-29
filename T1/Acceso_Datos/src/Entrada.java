1b085ce4a0cff6c867536210b5ae499c179d0885
gago1210 <cristinagg96@hotmail.es>
2024-09-29 19:53:15 +0200

    primer ejercicio

---
+++ T1/Acceso_Datos/src/Entrada.java
@@ -0,0 +1,13 @@
+ public class Entrada {
+
+//para que este codigo no de fallo con solo esta parte, se debe cumplir, en concreto la palabra entrada, es
+// ir en mayusculas la primera con matusculas y tal y como se nombre el fichero, debe ser el mismo, es decir,
+// la clase publica tiene que coincidir con el nombre del fichero. si lo que quiero hacer es una clase privada
+// primero tiene que haber una publica.//
+public static void main(String[] args) {
+    //static: es un metodo estatico, que a su vez significa que es un metodo que pertenece a la clase y referirse
+    //          al objeto, referencia a la clase en otro lugar que lo queramos utilizar//
+    System.out.println("ejemplo de mensaje");
+    //aqui vamos a mostrar lo que este en el parentesis//
+}
+}
