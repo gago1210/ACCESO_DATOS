41d6eb30db9c11595cd170d8b39c8a1ed766c86f
gago1210 <cristinagg96@hotmail.es>
2024-11-08 20:13:12 +0100

    clase 20/09

--- T1/Fichero/src/Entrada.java
+++ T1/Fichero/src/Entrada.java
@@ -1,16 +1,42 @@
 import controller.GestorFicheros;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.FileNotFoundException;
 
 public class Entrada {
-    public static void main(String[]args){
+    public static void main(String[]args) {
+
+// al ponerle  public static void main(String[]args) throws FileNotFoundException,
+// le paso toda la responsabilidad  al metodo main, ESTO TIENE QUE SER LA ULTIMA OPCION A REALIZAR
+// EL PROGRAMA SE ME VA  APARAR SI HAY ALGUNA EXCEPCION QUE NO SE SOLUCIONE
+
 
         GestorFicheros gestorFicheros = new GestorFicheros();
         //gestorFicheros.lecturaDirectorios ("src/resources/directorio");
         //gestorFicheros.lecturaDirectorios ("src/resources/ficheros");
+        //gestorFicheros.lecturaRecursiva("C:\\Users\\GAGO\\Documents\\GitHub");
+        gestorFicheros.lecturaTextoPlano("src/resources/directorio/subcarpeta/ficheros/lectura.txt");
 
-        gestorFicheros.lecturaRecursiva("C:\\Users\\GAGO\\Documents\\GitHub");
+        //instancias de objetos y llamada sa metodos
+        //OBJETO CLASE 1
+        //OBJETO CLASE 2
+        //FILE --> fichero logico --> fisico
+        //fichero logico
+        //File ficheroSinpUNTERO = new File ("C:\\Users\\GAGO\\Documents\\GitHub");
+        //System.out.println(ficheroSinPuntero.getName());
+        //System.out.println(ficheroSinPuntero.getParent());
+        //System.out.println(ficheroSinPuntero.length());
+        //System.out.println(ficheroSinPuntero.exists());
+        //System.out.println(ficheroSinPuntero.isDirectory());
+
+        //File[] -->todas las rutas de los FICHEROS que estan dentro del directorio ficheroSinPUnerto.list();
+
+        //EJERCICIO 1: Obtener todos los nombres de los ficheros del directorio llamado directorio
+        //EJERCICIO 2: Crear una subcarpeta en el directorio llamado directorio y crea ren ella un fichero.
+                    //Obtener todos los nombres de los ficheros del directorio llamado directorio y el dubdirectorio creado.
+        //EJERCICIO 3. Listar el nombre de todos los ficheros del SISTEMA (C://Users)
+
+        //RECURSIVIDAD
+
 
 
     }
--- T1/Fichero/src/controller/GestorFicheros.java
+++ T1/Fichero/src/controller/GestorFicheros.java
@@ -1,5 +1,9 @@
 package controller;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class GestorFicheros {
 
     public void lecturaDirectorios (String path)   {
@@ -10,11 +14,11 @@ public class GestorFicheros {
         for (File item : ficheros ) {
             //if (!item.isHidden()){ -->solo mostrar los que no son ocultos
                 System.out.println(item.getName ()); //hasta aqui la parte del ejercicio 1, comienzo del 2:
-                if  (item.isDirectory()){
+                if  (item.isDirectory()) {
                     File[] subdirectorio = item.listFiles();
                     for (File file1 : subdirectorio) {
                         System.out.println(file1.getName()); //hasta aqui el ejercicio 2,
-                        // comienzo del 3, lo va a hacer de sollo lo de la carpeta de GitHub
+                        // comienzo del 3, lo va a hacer de solo lo de la carpeta de GitHub
                     }
             }
         }
@@ -23,7 +27,7 @@ public class GestorFicheros {
         File file = new File(path); //paso a un fichero logico y fisico
         File[] ficheros = file.listFiles();
         for (File item : ficheros) {
-            System.out.println((item.getName());
+            System.out.println(item.getName());
             //pregunto si es directorio
             //saco todos los ficheros del directorio y los muestro --> no se cuantas veces pregunto --> aplico la recursividad
             //Recursividad es una tecnica de ejecucion que se llama a ella misma, teniendo en cuidado en que tenga una salida:
@@ -38,4 +42,50 @@ public class GestorFicheros {
             }
         }
     }
+
+   // CLASE 20/09 LECTURA DE FICHEROS
+    public void lecturaTextoPlano (String path) {
+        //creo el File -->FileReader --> finally, cuando se terminan SE CIERRAN con finally (ver abajo)
+        File file = new File (path);
+        fileReader fileReader = null;
+        BufferedReader buffererReader = null;
+
+        if (file.exists() && file.isFile()) {
+            //existe y ademas es un fichero entonces:
+            //esta excepcion la puedo tratar aqui porque asi no pasa mas adelante
+            try {
+                fileReader = new FileReader(file); //esto se traduce a no se ha encontrado el fichero por lo que pasa a catch
+                //hacer una lectura recurrente (frase, no letra por letra)
+                //mientras exista el numero hace la lectura e imprime
+                while ((lectura = fileReader.read())!=-1) {
+                    //imprime
+                    System.out.print((char)lectura); //quito ln a println y me lee todo lo del fichero lectura
+                    //este tipo de lectura nos interesa para leer todo
+                }
+
+                /*int lectura = fileReader.read(); //nos da un numero entero --> que se traduce a un caracter UNICODE correspondiente a una letra en el codigo  ASCI
+                System.out.println((char) lectura);
+                lectura = fileReader.read();
+                System.out.println((char)lectura);
+                lectura = fileReader.read();
+                System.out.println((char)lectura);
+                BufferedReader = new BufferedReader(fileReader);*/ //esto no nos sirve para hacer una lectura recurrente
+
+
+            } catch (FileNotFoundException e) {
+                System.out.println("Fallo en la lectura del fichero");
+                System.out.println(e.getMessage());
+            } catch (IOException e){
+                System.out.println("Error en la escritura, fallo de permisos");
+            } finally {
+                //es un bloque que se ejecuta siempre, si o si, haya fallos o no
+                fileReader.close (); //aqui hay qu eponer otra excepcion, si no nos va a dar error del tipo IOException, lo arreglamos
+            } catch (IOException e) {
+                System.out.println("Error en el cerrado del flujo");
+            }
+        }
+    }
+
+
+
 }
